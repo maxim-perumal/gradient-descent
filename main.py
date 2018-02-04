0e9dd6307449c7f1a091e767e9f5ba656308dfa3
Maxime Coutté <coutte.maxime@gmail.com>
2018-02-04 12:46:36 +0100

    always find a minima
    The previous version doing 20 derivative calculation, this one stop when a minima is found.

--- main.py
+++ main.py
@@ -3,22 +3,24 @@ from math_function import *
 
 f1 = polynomial_deg4(1,1,-13,-1,12)
 
-random_point = [random.randint(-10,10),random.randint(-10,10),random.randint(-10,10),random.randint(-10,10)]
+random_point = [-5,0,5]
 j = 0
 
 while (j < len(random_point)):
     step = 1
     previous_state = -1 # -1 mean no previous state
 
-    for i in range(0,20) :
+    i = 0
+
+    while(step > 0.01) :
         if (f1.derivative(random_point[j]) > 0):
             if (previous_state == 0):
-                step = step/2
+                step -= step/10
             random_point[j] -= step
             previous_state = 1
         elif (f1.derivative(random_point[j]) < 0):
             if (previous_state == 1):
-                step = step/2
+                step += step/10
             random_point[j] += step
             previous_state = 0
     j += 1
